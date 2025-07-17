d83437ddea7952b74f11c1203c0744bc9a5fc472
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-07-17 21:50:20 +0530

    Winner Program

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -14,6 +16,11 @@ public class GameManager : NetworkBehaviour
         public PlayerType playerType;
     }
     public event EventHandler OnGameStarted;
+    public event EventHandler<OnGameWinEventArgs> OnGameWin;
+    public class OnGameWinEventArgs : EventArgs
+    {
+        public Line line;
+    }
     public event EventHandler OnCurrentPlayablePlayerTypeChanged;
     public enum PlayerType
     {
@@ -21,9 +28,24 @@ public class GameManager : NetworkBehaviour
         Cross,
         Circle,
     }
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical,
+        DiagonalA,
+        DiagonalB,
+    }
+    public struct Line
+    {
+        public List<Vector2Int> gridVector2IntList;
+        public Vector2Int centerGridPosition;
+        public Orientation orientation;
+    }
 
     private PlayerType localPlayerType;
     private NetworkVariable<PlayerType> currentPlayablePlayerType = new NetworkVariable<PlayerType>();
+    private PlayerType[,] playerTypeArray;
+    private List<Line> lineList;
     private void Awake()
     {
         if (Instance != null)
@@ -31,6 +53,90 @@ public class GameManager : NetworkBehaviour
             Debug.LogError("Another GameManager");
         }
         Instance = this;
+
+        playerTypeArray = new PlayerType[3,3];
+
+        lineList = new List<Line>
+        {
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(0,0), new Vector2Int(1,0), new Vector2Int(2,0),
+                },
+                centerGridPosition = new Vector2Int(1,0),
+                orientation = Orientation.Horizontal,
+            },
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(0,1), new Vector2Int(1,1), new Vector2Int(2,1),
+                },
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.Horizontal,
+            },
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(0,2), new Vector2Int(1,2), new Vector2Int(2,2),
+                },
+                centerGridPosition = new Vector2Int(1,2),
+                orientation = Orientation.Horizontal,
+            },
+            //
+
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(0,0), new Vector2Int(0,1), new Vector2Int(0,2),
+                },
+                centerGridPosition = new Vector2Int(0,1),
+                orientation = Orientation.Vertical,
+            },
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(1,0), new Vector2Int(1,1), new Vector2Int(1,2),
+                },
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.Vertical,
+            },
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(2,0), new Vector2Int(2,1), new Vector2Int(2,2),
+                },
+                centerGridPosition = new Vector2Int(2,1),
+                orientation = Orientation.Vertical,
+            },
+
+            //
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(0,0), new Vector2Int(1,1), new Vector2Int(2,2),
+                },
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.DiagonalA,
+            },
+            new Line
+            {
+                gridVector2IntList = new List<Vector2Int>
+                {
+                    new Vector2Int(0,2), new Vector2Int(1,1), new Vector2Int(2,0),
+                },
+                centerGridPosition = new Vector2Int(1,1),
+                orientation = Orientation.DiagonalB,
+            },
+
+
+        };
     }
 
     public override void OnNetworkSpawn()
@@ -77,6 +183,11 @@ public class GameManager : NetworkBehaviour
         {
             return;
         }
+        if (playerTypeArray[x,y] != PlayerType.None)
+        {
+            return;
+        }
+        playerTypeArray[x, y] = playerType;
         OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs
         {
             x = x,
@@ -92,6 +203,37 @@ public class GameManager : NetworkBehaviour
             case PlayerType.Circle:
                 currentPlayablePlayerType.Value = PlayerType.Cross;
                 break;
+        }
+        TestWinner();
+    }
+    private bool TestWinnerLine(Line line)
+    {
+        return TestWinnerLine(
+            playerTypeArray[line.gridVector2IntList[0].x, line.gridVector2IntList[0].y],
+            playerTypeArray[line.gridVector2IntList[1].x, line.gridVector2IntList[1].y],
+            playerTypeArray[line.gridVector2IntList[2].x, line.gridVector2IntList[2].y]);
+    }
+    private bool TestWinnerLine(PlayerType aPlayerType, PlayerType bPlayerType, PlayerType cPlayerType)
+    {
+        return
+            aPlayerType != PlayerType.None &&
+            aPlayerType == bPlayerType &&
+            bPlayerType == cPlayerType;
+    }
+
+    private void TestWinner()
+    {
+        foreach (Line line in lineList)
+        {
+            if(TestWinnerLine(line))
+            {
+                currentPlayablePlayerType.Value = PlayerType.None;
+                OnGameWin?.Invoke(this, new OnGameWinEventArgs
+                {
+                    line = line
+                });
+                break;
+            }
         }
     }
     
