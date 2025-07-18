500fff62f8a5e512d2be4cf2568635831d663ff6
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-07-18 12:49:35 +0530

    Add Game Over UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using static GameManager;
 
 public class GameManager : NetworkBehaviour
 {
@@ -20,6 +21,7 @@ public class GameManager : NetworkBehaviour
     public class OnGameWinEventArgs : EventArgs
     {
         public Line line;
+        public PlayerType winPlayerType;
     }
     public event EventHandler OnCurrentPlayablePlayerTypeChanged;
     public enum PlayerType
@@ -223,20 +225,28 @@ public class GameManager : NetworkBehaviour
 
     private void TestWinner()
     {
-        foreach (Line line in lineList)
+        for(int i = 0; i < lineList.Count; i++)
         {
+            Line line = lineList[i];
             if(TestWinnerLine(line))
             {
                 currentPlayablePlayerType.Value = PlayerType.None;
-                OnGameWin?.Invoke(this, new OnGameWinEventArgs
-                {
-                    line = line
-                });
+                TriggerOnGameWinRpc(i, playerTypeArray[line.centerGridPosition.x,line.centerGridPosition.y]);
                 break;
+
             }
         }
     }
-    
+    [Rpc(SendTo.ClientsAndHost)]
+    private void TriggerOnGameWinRpc(int lineIndex, PlayerType winPlayerType)
+    {
+        Line line = lineList[lineIndex];
+        OnGameWin?.Invoke(this, new OnGameWinEventArgs
+        {
+            line = line,
+            winPlayerType = winPlayerType
+        });
+    }
     public PlayerType GetLocalPlayerType()
     {
         return localPlayerType;
